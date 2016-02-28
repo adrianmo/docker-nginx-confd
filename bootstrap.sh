3a46cc20eac65dc7f6942fafa2f6080bb0b95f27
Adrian Moreno <adrian@morenomartinez.com>
2016-02-28 02:22:04 +0100

    Adds confd config file

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 set -e
 
+TOML=/etc/confd/conf.d/nginx.toml
+
 if [ -z "$CONFD_BACKEND" ]; then
   echo "CONFD_BACKEND environment variable not set. Exiting..."
   exit 1
@@ -13,17 +15,20 @@ if [ "$CONFD_BACKEND" == "etcd" ]; then
   fi
 
   #launch it one time to see if it fails
-  confd -onetime -backend etcd -node http://$ETCD_NODE
+  confd -onetime -backend etcd -node http://$ETCD_NODE -config-file ${TOML}
   if [ $? != 0 ]; then
     echo "Error running confd"
     exit 1
   fi
 
   confd -interval 10 -backend etcd -node http://$ETCD_NODE &
+  echo "[nginx] confd is now monitoring etcd for changes..."
 
 else
   echo "confd backend not supported: $CONFD_BACKEND"
   exit 1
 fi
 
-/usr/sbin/nginx -g 'daemon off;'
+# Start the Nginx service using the generated config
+echo "[nginx] starting nginx service..."
+exec /usr/sbin/nginx -g 'daemon off;'
